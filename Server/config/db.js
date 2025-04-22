2252f0888ee2e5cbacf29042a790ff58927d7618
Vipin Kumar <vipinch211@gmail.com>
2025-04-22 12:07:57 +0530

    Changes

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 dotenv.config();
-const URL=process.env.MONGO_URL || "mongodb://localhost:27017/useappDB"
+const URL=process.env.MONGO_URL
 const dbConnect=async()=>{
     try{
        await mongoose.connect(URL);
